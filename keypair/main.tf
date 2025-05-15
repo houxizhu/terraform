54791cdf4bcb2e1a19593b0748749cc4c2fd2fea
adevopsbeginner <adevopsbeginner@gmail.com>
2025-05-15 11:11:15 +1000

    keypair

--- keypair/main.tf
+++ keypair/main.tf
@@ -7,7 +7,7 @@ resource "tls_private_key" "ed25519" {
   algorithm = "ED25519"
 }
 
-resource "aws_key_pair" "t_abeginner" {
+resource "aws_key_pair" "this" {
   key_name   = "${var.this_name}_ed25519"
   public_key = tls_private_key.ed25519.public_key_openssh
 }
